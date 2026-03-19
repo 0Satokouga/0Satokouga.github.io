23ce0c4385acc18746167879d0587f9cc7045bb0
0Satokouga <deathaurayt@gmail.com>
2026-03-19 08:05:56 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+const section = document.getElementById("hSection");
+
+//Hide
+section.onclick = function(){
+    section.style.display="none";
+    let a = 1;
+};
+
+//Show
+function showsection(){
+    section.style.display="block";
+    let a = 0;
+}
